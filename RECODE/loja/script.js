2f958d9ef025de00c6aadcaea3d234e1bb2ca7cb
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-02-11 11:13:23 -0300

    filtro loja

--- RECODE/loja/script.js
+++ RECODE/loja/script.js
@@ -1,3 +1,13 @@
 function mostrarcat(categoria){
-    window.alert(categoria);
+    //window.alert(categoria);
+    let elemento = document.getElementsByClassName('boxProduto');
+    //console.log(elemento);
+    for(var i=0;i<elemento.length;i++){
+        //console.log(elemento[i].id);
+        if(elemento[i].id != categoria){
+            elemento[i].style = 'display:none'
+        }else{
+            elemento[i].style = 'display:inline-block'
+        }
+    }
 };
